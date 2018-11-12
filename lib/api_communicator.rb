e3975df19a21b2921aa0dcb66e31ee735532c980
GermainDuran <arturodurantorres@gmail.com>
2018-11-12 15:30:22 -0500

    done

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -6,8 +6,27 @@ def get_character_movies_from_api(character)
   #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
-  
-  # NOTE: in this demonstration we name many of the variables _hash or _array. 
+  films_character = []
+  response_hash["results"].each do |character_hash|
+    if character_hash["name"].downcase== character
+      films_character << character_hash["films"]
+    end
+  end
+  films_character.flatten!
+
+  # collect those film API urls, make a web request to each URL to get the info
+  #  for that film
+  # return value of this method should be collection of info about each film.
+  #  i.e. an array of hashes in which each hash reps a given film
+  films_hash=films_character.map do |url|
+    response= RestClient.get("#{url}")
+    response_2= JSON.parse(response)
+
+  end
+    films_hash
+
+
+  # NOTE: in this demonstration we name many of the variables _hash or _array.
   # This is done for educational purposes. This is not typically done in code.
 
 
@@ -17,6 +36,7 @@ def get_character_movies_from_api(character)
   #  for that film
   # return value of this method should be collection of info about each film.
   #  i.e. an array of hashes in which each hash reps a given film
+
   # this collection will be the argument given to `parse_character_movies`
   #  and that method will do some nice presentation stuff: puts out a list
   #  of movies by title. play around with puts out other info about a given film.
@@ -24,6 +44,14 @@ end
 
 def print_movies(films_hash)
   # some iteration magic and puts out the movies in a nice list
+  films_title=films_hash.map do |films|
+    films["title"]
+  end
+  counter = 0
+  films_title.each do |title|
+    counter +=1
+    puts "Title #{counter}: #{title}"
+  end
 end
 
 def show_character_movies(character)
